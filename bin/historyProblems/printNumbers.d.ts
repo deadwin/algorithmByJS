a5845cb98cd11eb7d710c0d9d1a39d3ff606653e
deadwin <837115602@qq.com>
2020-11-29 11:46:19 +0800

    剑指 Offer 17. 打印从 1 到最大的 n 位数

---
+++ bin/historyProblems/printNumbers.d.ts
@@ -0,0 +1,9 @@
+/**剑指 Offer 17. 打印从 1 到最大的 n 位数
+ * 2020年11月29日
+ * @param n
+ */
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+declare var printNumbers: (n: number) => number[];
---
+++ src/historyProblems/printNumbers.ts
@@ -0,0 +1,23 @@
+/**剑指 Offer 17. 打印从 1 到最大的 n 位数
+ * 2020年11月29日
+ * @param n 
+ */
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var printNumbers = function(n:number):number[] {
+    let arr = [];
+    function list(n){
+        if(n == 0) return;
+        list(n - 1);
+        arr.push(n);
+    }
+    let num = 0;
+    for(let i = 0 ;i < n;i ++){
+        num *= 10;
+        num += 9;
+    }
+    list(num)
+    return arr;
+};
