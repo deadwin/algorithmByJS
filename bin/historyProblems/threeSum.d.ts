21e4a9a6db6da0ced86d196324bbeca9644b7d4b
altMan <altman@altman.com>
2022-02-10 03:06:09 +0800

    5. 三数之和

---
+++ bin/historyProblems/threeSum.d.ts
@@ -0,0 +1,5 @@
+/**5. 三数之和
+ * 2022年2月10日
+ * 非最优解
+ */
+export declare function threeSum(nums: number[]): number[][];
---
+++ src/historyProblems/threeSum.ts
@@ -0,0 +1,39 @@
+/**5. 三数之和
+ * 2022年2月10日
+ * 非最优解
+ */
+export function threeSum(nums: number[]): number[][] {
+    if(!nums || nums.length < 3) return [];
+    let ret:number[][]= [];
+    function check(n_arr:Array<number>){
+        for(let i =0;i <ret.length;i++){
+            let arr = ret[i];
+            if(n_arr[0] == arr[0] && n_arr[1] == arr[1] && n_arr[2] == arr[2]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    let map:Map<number,number> = new Map();
+    map.set(nums[0],0);
+    for(let i =1;i < nums.length -1;i++){
+        let num1= nums[i];
+        for(let j = i+1;j < nums.length;j++){
+            let num2= nums[j];
+            let remain = 0 - num1 - num2;
+            let has = map.has(remain);
+            if(has){
+                let ans = [num1,num2,remain];
+                ans.sort((a,b)=>{
+                    return a-b;
+                })
+                if(check(ans)){
+                    ret.push(ans);
+                }
+            }
+        }
+        map.set(num1,i);
+    }
+    return ret;
+};
