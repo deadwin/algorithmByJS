71e578e122528f442c0d0dfacc81026148c9a51f
altMan <altman@altman.com>
2022-02-12 04:35:38 +0800

    292. Nim 游戏

---
+++ bin/2022/canWinNim.js
@@ -0,0 +1,9 @@
+/**
+ * 292. Nim 游戏
+ * 2022年2月12日
+ * 要找到规律
+ */
+function canWinNim(n) {
+    return n % 4 != 0;
+}
+;
---
+++ src/2022/canWinNim.ts
@@ -0,0 +1,8 @@
+/**
+ * 292. Nim 游戏
+ * 2022年2月12日
+ * 要找到规律
+ */
+function canWinNim(n: number): boolean {
+    return n % 4 != 0;
+};
