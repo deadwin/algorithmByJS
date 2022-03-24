2f3464108f494ff3b6c0879fcf506660c40ffe40
altMan <altman@altman.com>
2022-03-25 00:29:13 +0800

    100. 相同的树

--- bin/historyProblems/isSameTree.js
+++ bin/historyProblems/isSameTree.js
@@ -1,10 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function isSameTree(p, q) {
-    if (!p && !q)
-        return true;
     if (p && q) {
         return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+    else if (!p && !q) {
+        return true;
     }
     else {
         return false;
--- src/historyProblems/isSameTree.ts
+++ src/historyProblems/isSameTree.ts
@@ -15,9 +15,10 @@
 import { TreeNode } from "../problems/TreeNode";
 
 function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
-    if (!p && !q) return true;
     if (p && q) {
         return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    } else if (!p && !q) {
+        return true;
     } else {
         return false;
     }
