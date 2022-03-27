5271a118fd9733afa64d877d69b4ece7e662f87c
altMan <altman@altman.com>
2022-03-28 03:08:47 +0800

    剑指 Offer 18. 删除链表的节点　递归解法

--- bin/historyProblems/deleteNode.js
+++ bin/historyProblems/deleteNode.js
@@ -14,3 +14,24 @@ var deleteNode = function (node) {
     node.val = node.next.val;
     node.next = node.next.next;
 };
+/**剑指 Offer 18. 删除链表的节点 2020年8月29日
+ * 采用递归解
+ *
+ * @param head
+ * @param val
+ * @returns
+ */
+function deleteNode2(head, val) {
+    function a(list, val) {
+        if (list == null)
+            return;
+        if (list.val == val) {
+            return list.next ? list.next : null;
+        }
+        let node = a(list.next, val);
+        list.next = node;
+        return list;
+    }
+    return a(head, val);
+}
+;
--- src/historyProblems/deleteNode.ts
+++ src/historyProblems/deleteNode.ts
@@ -11,7 +11,27 @@
  * @param {ListNode} node
  * @return {void} Do not return anything, modify node in-place instead.
  */
-var deleteNode = function(node:ListNode):void {
+var deleteNode = function (node: ListNode): void {
     node.val = node.next.val;
     node.next = node.next.next;
+};
+
+/**剑指 Offer 18. 删除链表的节点 2020年8月29日
+ * 采用递归解
+ * 
+ * @param head 
+ * @param val 
+ * @returns 
+ */
+function deleteNode2(head: ListNode | null, val: number): ListNode | null {
+    function a(list: ListNode, val: number) {
+        if (list == null) return;
+        if (list.val == val) {
+            return list.next ? list.next : null;
+        }
+        let node = a(list.next, val);
+        list.next = node;
+        return list;
+    }
+    return a(head, val);
 };
