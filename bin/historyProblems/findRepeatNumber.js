da7d0ee86be7f300d6322d86fc66fc12f5d6a9fa
altMan <altman@altman.com>
2022-03-28 02:31:02 +0800

    剑指 Offer 03. 数组中重复的数字

---
+++ bin/historyProblems/findRepeatNumber.js
@@ -0,0 +1,15 @@
+/**剑指 Offer 03. 数组中重复的数字 日期
+ * 目前空间复杂度是O(N),还可以优化到O(1)
+ * @param nums
+ * @returns
+ */
+function findRepeatNumber(nums) {
+    let set = new Set();
+    for (let i = 0; i < nums.length; i++) {
+        if (set.has(nums[i])) {
+            return nums[i];
+        }
+        set.add(nums[i]);
+    }
+}
+;
