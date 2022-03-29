35236b57e2ff89cf78b9921a9d776c6524bdbf46
altMan <altman@altman.com>
2022-03-30 02:01:17 +0800

    389. 找不同  有好几种解法，目前用哈希不是完美解

---
+++ bin/historyProblems/findTheDifference.d.ts
@@ -0,0 +1,7 @@
+/**389. 找不同　2022年3月30日
+ *
+ * @param s
+ * @param t
+ * @returns
+ */
+declare function findTheDifference(s: string, t: string): string;
---
+++ src/historyProblems/findTheDifference.ts
@@ -0,0 +1,39 @@
+/**389. 找不同　2022年3月30日
+ * 
+ * @param s 
+ * @param t 
+ * @returns 
+ */
+function findTheDifference(s: string, t: string): string {
+    return sol1(s, t);
+};
+/**解法1,使用哈希 */
+function sol1(s: string, t: string) {
+    let len = s.length;
+    let set = {};
+    for (let i = 0; i < len; i++) {
+        let s1 = s[i];
+        let s2 = t[i];
+        if (set[s1]) {
+            set[s1] = false;
+        } else {
+            set[s1] = true;
+        }
+        if (set[s2]) {
+            set[s2] = false;
+        } else {
+            set[s2] = true;
+        }
+    }
+    let last = t[len];
+    if (set[last]) {
+        set[last] = false;
+    } else {
+        return last;
+    }
+    for (let v in set) {
+        if (set[v]) {
+            return v;
+        }
+    }
+}
