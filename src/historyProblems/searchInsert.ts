1d090944ca8fafbd61bee657a1b0120ca1c43fcf
altMan <altman@altman.com>
2022-03-31 01:22:22 +0800

    35. 搜索插入位置　简单复习

--- src/historyProblems/searchInsert.ts
+++ src/historyProblems/searchInsert.ts
@@ -1,26 +1,44 @@
 //35. 搜索插入位置
 
 export function searchInsert(nums: number[], target: number): number {
-    if(target < nums[0]){
-        return 0;
-    }else if(target > nums[nums.length - 1]){
-        return nums.length;
-    }
+    return searchInsert2(nums, target);
+    // if (target < nums[0]) {
+    //     return 0;
+    // } else if (target > nums[nums.length - 1]) {
+    //     return nums.length;
+    // }
+    // let min = 0;
+    // let max = nums.length - 1;
+    // while (min <= max) {
+    //     let mid = Math.floor((min + max) / 2);
+    //     let midNum = nums[mid];
+    //     if (midNum < target) {
+
+    //         min = mid + 1;
+    //     } else if (midNum > target) {
+
+    //         max = mid - 1;
+    //     } else {
+    //         return mid;
+    //     }
+
+    // }
+    // return min;
+};
+function searchInsert2(nums, target) {
     let min = 0;
     let max = nums.length - 1;
-    while(min <= max){
-        let mid = Math.floor((min + max) / 2);
-        let midNum = nums[mid];
-        if(midNum < target){
-
-            min = mid + 1;
-        }else if(midNum > target){
-
-            max = mid - 1;
-        }else{
+    let mid;
+    while (min <= max) {
+        mid = Math.floor((min + max) / 2);
+        if (nums[mid] == target) {
             return mid;
         }
-        
+        if (nums[mid] < target) {
+            min = mid + 1;
+        } else {
+            max = mid - 1;
+        }
     }
     return min;
-};
+}
